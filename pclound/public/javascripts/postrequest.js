db868223de65dce7eca8a1415cd8cacd39f19766
Marcos Vinicius <viniciushkd@gmail.com>
2018-10-26 21:26:08 -0300

    Trabalho Cloud IO

---
+++ pclound/public/javascripts/postrequest.js
@@ -0,0 +1,34 @@
+$( document ).ready(function() {
+    
+    $("#customerForm").submit(function(event) {
+		event.preventDefault();
+		ajaxPost();
+	});
+    
+    function ajaxPost(){
+
+    	$.ajax({
+			type : "POST",
+			contentType : "application/json",
+			url : "http://localhost:8080/twitter?tweet=" + $("#pesquisa").val(),
+            dataType : 'json',
+            timeout: 150000,
+			success : function(customer) {
+                console.log(customer);
+                $('.list-group-item').remove();
+                $.each(customer, function(index, value){
+                    $('#listaTwitter').append('<li class="list-group-item">'+value+'</li>');
+                });
+			},
+			error : function(e) {
+				alert("Error!")
+				console.log("ERROR: Verifique se o servidor REST esta no ar");
+			}
+        });
+    	reset();
+    }
+    
+    function reset(){
+    	$("#pesquisa").val("");
+    }
+});
